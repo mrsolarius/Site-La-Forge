10d14c4cda3f75a4fac8545de813e7dbd01ace19
antbogir <48952161+antbogir@users.noreply.github.com>
2019-03-26 15:27:25 +0100

    Create navbar.php

---
+++ template/navbar.php
@@ -0,0 +1,14 @@
+<?php
+<nav>
+            <ul>
+                <li><a href="Accueil.html">Accueil</a></li>
+                <li><a href="Présentation.html">Présentation</a></li>
+                <li><a href="Nos couteaux.html">Nos couteaux</a></li>
+                <li>Actualités</li>
+                <li>Galerie</li>
+                <li>Infos Pratiques</li>
+                <li>Se connecter / S'inscrire</li>
+            </ul>
+</nav>
+?>
+
