f7b4050ae1c5265e52152ed43163f3b6a126c0a9
mrsolarius <louilouiticien.site@gmail.com>
2020-01-14 20:25:17 +0100

    Infos_pratiques.php 	Nos_couteaux.php 	Presentation.php 	Script Insertion.sql 	checkout.php 	checkoutstep2.php 	checkoutstep3.php 	checkoutvalide.php 	commandes.php 	css/Acceuil.css 	css/Nos_couteaux.css 	css/Presentation.css 	css/Produit.css 	css/cartebleu.css 	css/defaut.css 	css/footer.css 	css/form.css 	css/maincss.css 	css/panier.css 	css/produits.css 	css/profile.css 	dashboard/ 	disconectprosse.php 	editpasword.php 	editprofile.php 	img/ 	js/ 	legal/ 	passwordediteprocesse.php 	produit.php 	produits.php 	profile.php 	recap.php 	registerprocesse.php 	settings.php 	signin.php 	signinprocesse.php 	signup.php 	template/footer.php 	template/head.php 	template/panier.php 	updateProfile.php

--- index.php
+++ index.php
@@ -4,14 +4,75 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+session_start();
+ $location = basename(__FILE__);
+ include ($_SERVER['DOCUMENT_ROOT'] . '/settings.php');
+ 
+?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>salut</title>
-        <link rel="stylesheet" href="css/navbar.css"/>
+        <title>La Forge Au Couteau</title>
+        <link href="css/Acceuil.css" rel="stylesheet" type="text/css"/>
+        <?php include ('./template/head.php');?>
+
     </head>
-    <body>
+    <body>       
+        <?php include ('./template/navbar.php'); ?>
+        <?php include ('./template/panier.php'); ?>
+        <?php 
+        echo '<p>'; var_dump($_SESSION); echo '</p>';
+        ?>    
+        <div class="bgimg-1" >
+            <div class="caption">
+                <span class="border">LA FORGE AU COUTEAU</span>
+            </div>
+        </div>
         
-        <?php include ('/template/navbar.php'); ?>
+        
+        
+        <div id="stopbar" style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+            <h3 style="text-align:center;">Couteau Forger</h3>
+            <p>Tous nos couteaux sont fabriqués de manière artisanale en Savoie. Les lames sont forgées dans des aciers de qualité, et les manches sont façonnés en bois massifs ou en marqueterie de plusieurs bois. Les lames sont laissées brutes de forge sur le dos, ce qui donne un style rustique et moderne. Chaque couteau est étudié pour être le plus performant selon les utilisations. Les couteaux de cuisine sont forgés très fin et léger pour avoir une coupe exceptionnelle, tandis que les couteaux de camp sont plus lourd et plus épais. Les couteaux pliants de type piémontais sont assez polyvalent. Les couteaux utilitaires sont destinés à une utilisation quotidienne (table, cuisine, randonnée...)</p>
+        </div>
+        <div class="bgimg-2">
+        </div>
+        
+    
+        <div id="canava1">
+            <div class="accColum">
+            <h3 style="text-align:center;color:white">
+                Unique</h3>
+            <p style="color:#ffffffc9">Chaque couteau est réalisé de manière artisanale. Tout est entièrement fait à la main sans gabarit, ce qui rend chaque couteau unique. Les lames sont forgées et les manches sont façonnés en bois, ou en corne.</p>
+            </div>
+            
+            <div class="accColum">
+            <h3 style="text-align:center;color:white">Sur mesure</h3>
+            <p style="color:#ffffffc9">Chaque couteau peut être réalisé selon vos souhait. La forme de la lame ou du manche. Du bois massif ou en marqueterie.Réalisons ensemble votre projet. </p>
+            </div>
+        </div>
+
+        <div class="bgimg-3">
+            
+        </div>
+
+        <div style="position:relative;">
+            <h3 style="text-align:center;">Nos Lames</h3>
+            <div style="color:#777;background-color:white;text-align:center;padding:0 80px 50px;text-align: justify;">
+                <p>Différents aciers sont utilisés pour la création des couteaux: XC75, XC100, 100C6, 14C28N (inoxydable)...
+                Ce sont des aciers à haute teneur en carbone spécialement étudié pour la coutellerie et qui garantissent un excellent pouvoir de coupe sur le long terme. 
+                Toutes les lames sont laissées brutes de forge sur le haut, et sont polies au niveau du tranchant.</p>
+            </div>
+        </div>
+
+        <div class="bgimg-1">
+            <div class="caption">
+                <span class="border">COOL!</span>
+            </div>
+        </div>
+        
+        <?php include ('./template/footer.php'); ?>
+
     </body>
 </html>
--- template/navbar.php
+++ template/navbar.php
@@ -1,14 +1,70 @@
-<?php
-<nav>
-            <ul>
-                <li><a href="Accueil.html">Accueil</a></li>
-                <li><a href="Présentation.html">Présentation</a></li>
-                <li><a href="Nos couteaux.html">Nos couteaux</a></li>
-                <li>Actualités</li>
-                <li>Galerie</li>
-                <li>Infos Pratiques</li>
-                <li>Se connecter / S'inscrire</li>
-            </ul>
-</nav>
-?>
 
+
+<nav class="topnav" id="bardenavigation" role="navigation">
+    <a class="active" id="titre" href="/index.php">La Forge Au <span>Couteau</span></a>
+  <a <?php if($location==='index.php'){ echo 'class="active"';} ?> href="/index.php">Accueil</a>
+  <a <?php if($location==='Presentation.php'){ echo 'class="active"';} ?> href="/Presentation.php">Présentation</a>
+  <a <?php if($location==='Nos_couteaux.php'){ echo 'class="active"';} ?> href="/Nos_couteaux.php">Nos couteaux</a>
+  <a onclick="panier();" id="toogle-panier">
+      <span>
+          <?php 
+            if (isset($_SESSION['idclient'])) {
+                $idCli = $_SESSION['idclient'];
+                if (isset($idCli)) {
+                    $dbconn = pg_connect("host=$dbhost dbname=$dbbd user=$dbuser password=$dbpassword")
+                    or die('Connexion impossible : ' . pg_last_error());
+                    $selectQ = "Select sum(qte) from panier where id_client =" . $idCli;
+                    $selectres = pg_query($selectQ) or die('Échec de la requête : ' . pg_last_error());
+                    $panier = pg_fetch_array($selectres, null, PGSQL_ASSOC);
+                    $sum = (int)$panier['sum'];
+                    if($sum>=1){
+                        echo $panier['sum'];
+                    }                    
+                }
+            }
+          ?>
+      </span>🛒
+      </a>
+  <?php
+  if(isset($_SESSION['prenom'])){
+      if($_SESSION['admin']=='t'){
+           echo '<div class="dropdown">
+                <button class="dropbtn"><a>Bonjour  '.$_SESSION['prenom'].
+                    '</a> <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="/dashboard/dashboard.php">Dashboard</a>
+                    <a href="/profile.php">Profil</a>
+                    <a href="/commandes.php">Mes Commandes</a>
+                    <a href="disconectprosse.php" id="disconect">Se déconnecter</a>
+                </div>
+            </div>';     
+      }else{
+          echo '<div class="dropdown">
+                    <button class="dropbtn">Bonjour  '.$_SESSION['prenom'].
+                        ' <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="/profile.php">Profil</a>
+                        <a href="/commandes.php">Mes Commandes</a>
+                        <a href="disconectprosse.php" id="disconect">Se déconnecter</a>
+                    </div>
+                </div>';
+      }
+  }else{
+      if($location==='signin.php' || $location==='signup.php'){ 
+          $actif = 'class="active"';
+      }else{
+          $actif="";
+      } 
+      echo '<a style="float: right;"'.$actif.' href="/signin.php">Se connecter</a>';
+  }
+  ?>
+  <a onclick="menuResponsive();" class="icon">
+      <div id="nav-icon">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+  </a>
+</nav>
