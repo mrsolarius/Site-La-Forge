8c74c27e9a1553e20152bec7d83bb275f25577d7
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-01-02 15:26:58 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT',$_SERVER['DOCUMENT_ROOT']);
 session_start();
-$dbhost = '192.168.1.72';
+$dbhost = 'localhost';
 $dbbd = 'DBLaForge';
 $dbuser = 'postgres';
 $dbpassword = 'postgres';
